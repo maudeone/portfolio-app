de050462d74aead83d6f27114032804a6530fe48
Maude <ayoubbb973@gmail.com>
2021-12-07 11:22:27 -0500

    Header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar';
+import Particles from 'react-particles-js';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Particles
+      className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 60,
+              density: {
+                enable:true,
+                value_area:900
+              }
+            },  
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f2643d"
+              }
+            }
+          }  
+        }}
+      />
+      <Navbar />
+      <Header />
+    </div>  
   );
 }
 
